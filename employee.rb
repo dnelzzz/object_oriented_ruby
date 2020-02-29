19b2321971acfc05699eaa87c4bef9c72f3f5684
DARIN NELSON <nelson.darinp@gmail.com>
2020-02-29 06:48:47 -0600

    first commit

---
+++ employee.rb
@@ -0,0 +1,6 @@
+employee1 = ["James", "Franco", 1500000, true]
+employee2 = ["Jennifer", "Aniston", 10000000, false]
+puts employee1[0] + " " + employee1[1] + " makes " + employee1[2].to_s + " dollar per year." 
+puts employee2[0] + " " + employee2[1] + " makes " + employee2[2].to_s + " dollar per year." 
+puts "#{employee1[0]} #{employee1[1]} makes #{employee1[2]} per year."
+puts "#{employee2[0]} #{employee2[1]} makes #{employee2[2]} per year."
